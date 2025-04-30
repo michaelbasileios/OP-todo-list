8a4e1ce33fa6e2d45dbd4d06ce48486246084a32
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-04-30 12:53:12 -0500

    Refactor switch logic in generateHTML function to separate function

--- src/render.js
+++ src/render.js
@@ -6,42 +6,46 @@ function renderList(array) {
 function generateHTMLElement(todoListObject) {
     const todoItemCard = document.createElement("div");
     todoItemCard.classList.add('todoItemDiv');
-    for (const key in todoListObject) {
-        const value = todoListObject[key];
-
-        switch (key) {
-            case "Title":
-                const titleElement = document.createElement('h2');
-                titleElement.textContent = value;
-                todoItemCard.appendChild(titleElement);
-                break;
-            case "Description":
-                const descriptionElement = document.createElement('p');
-                descriptionElement.textContent = value;
-                todoItemCard.appendChild(descriptionElement);
-                break;
-            case "Priority":
-                const priorityElement = document.createElement('h3');
-                priorityElement.textContent = value;
-                todoItemCard.appendChild(priorityElement);
-                break;
-            case "Completion":
-                const completionStatusLabel = document.createElement('label');
-                completionStatusLabel.textContent = value ? "Complete" : "Incomplete";
-                const completionStatusElement = document.createElement('input');
-                completionStatusElement.type = 'checkbox';
-                completionStatusElement.name = 'completionStatusCheckbox';
-                completionStatusElement.checked = value ? true : false;
-                completionStatusLabel.append(completionStatusElement);
-                todoItemCard.append(completionStatusLabel);
-                break;
-            default:
-                const defaultElement = document.createElement('div');
-                defaultElement.textContent = `${key}: ${value}`;
-                todoItemCard.appendChild(defaultElement);
-        }
+    for (const property in todoListObject) {
+        const value = todoListObject[property];
+        propertyCheck(property, value, todoItemCard);
     }
     return todoItemCard;
+}
+
+//function to check the type of property of a todo item and create appropriate DOM elements
+function propertyCheck(property, value, todoItemCard) {
+    switch (property) {
+        case "Title":
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = value;
+            todoItemCard.appendChild(titleElement);
+            break;
+        case "Description":
+            const descriptionElement = document.createElement('p');
+            descriptionElement.textContent = value;
+            todoItemCard.appendChild(descriptionElement);
+            break;
+        case "Priority":
+            const priorityElement = document.createElement('h3');
+            priorityElement.textContent = value;
+            todoItemCard.appendChild(priorityElement);
+            break;
+        case "Completion":
+            const completionStatusLabel = document.createElement('label');
+            completionStatusLabel.textContent = value ? "Complete" : "Incomplete";
+            const completionStatusElement = document.createElement('input');
+            completionStatusElement.type = 'checkbox';
+            completionStatusElement.name = 'completionStatusCheckbox';
+            completionStatusElement.checked = value ? true : false;
+            completionStatusLabel.append(completionStatusElement);
+            todoItemCard.append(completionStatusLabel);
+            break;
+        default:
+            const defaultElement = document.createElement('div');
+            defaultElement.textContent = `${key}: ${value}`;
+            todoItemCard.appendChild(defaultElement);
+    }
 }
 
 export { renderList };
