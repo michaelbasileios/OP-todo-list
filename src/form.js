3ff11fcc0a73e96d082f17d74e812cb2e38322c5
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-05-02 16:49:59 -0500

    Add code for all form elements

---
+++ src/form.js
@@ -0,0 +1,48 @@
+function form() {
+    const todoForm = document.createElement('form');
+    todoForm.id = 'todo-form';
+    todoForm.method = 'dialog';
+    
+    const formTitleLabel = document.createElement('label');
+    const formTitleInput = document.createElement('input');
+    formTitleInput.type = 'text';
+    formTitleInput.classList = 'form-input-field';
+    formTitleInput.name = 'todo-title';
+    formTitleLabel.append(formTitleInput);
+
+    const formDescriptionLabel = document.createElement('label');
+    const formDescriptionInput = document.createElement('input');
+    formDescriptionInput.type = 'text';
+    formDescriptionInput.classList = 'form-input-field';
+    formDescriptionInput.name = 'todo-description';
+    formDescriptionLabel.append(formDescriptionInput);
+
+    const formPriorityLabel = document.createElement('label');
+    const formPriorityInput = document.createElement('input');
+    formPriorityInput.type = 'number';
+    formPriorityInput.classList = 'form-input-field';
+    formPriorityInput.name = 'todo-priority';
+    formPriorityLabel.append(formPriorityInput);
+
+    const formCompletionLabel = document.createElement('label');
+    const formCompletionCheckbox = document.createElement('input');
+    formCompletionCheckbox.type = 'checkbox';
+    formCompletionCheckbox.classList = 'form-input-field';
+    formCompletionCheckbox.name = 'todo-complete-status';
+    formCompletionLabel.append(formCompletionCheckbox);
+
+    const formSubmitBtn = document.createElement('button');
+    formSubmitBtn.id = 'submit-btn';
+    
+    const formCancelBtn = document.createElement('button');
+    formCancelBtn.id = 'cancel-btn';
+
+    todoForm.append(
+        formTitleLabel, 
+        formDescriptionLabel, 
+        formPriorityLabel, 
+        formCompletionLabel,
+        formSubmitBtn,
+        formCancelBtn
+        );
+}
