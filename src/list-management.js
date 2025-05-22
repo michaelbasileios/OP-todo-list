9a894410c6cc815f1ee9efd56397da8305ef6ccb
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-05-22 10:15:28 -0500

    Add code to create new project and push to array

--- src/list-management.js
+++ src/list-management.js
@@ -5,13 +5,13 @@ const listArray = [
         Title: "Clean Room",
         Description: "Make bed, dust off floor and surfaces",
         Completion: true,
-        Project: "Completed",
+        Project: "Default",
     },
     {
         Title: "Feed Dogs",
         Description: "Chicken",
         Completion: false,
-        Project: "Not Completed",
+        Project: "Default",
     },
 ];
 
--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,12 @@
+import { renderProjectList } from "./projects-render";
+
 const projectsArray = ["Default", "Completed", "Not Completed"];
 
 const getProjectsArray = () => projectsArray;
+
+function createNewProject(newProject) {
+    projectsArray.push(newProject);
+    renderProjectList(getProjectsArray());
+}
 
 export { getProjectsArray };
