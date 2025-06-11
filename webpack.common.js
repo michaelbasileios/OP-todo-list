5256bb6fb79782925900b5ae37dafd0cb9e4b7b2
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-06-11 14:57:08 -0500

    Create webpack modes

--- webpack.common.js
+++ webpack.common.js
@@ -3,17 +3,19 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/script.js",
+  // mode: "development",
+  entry: {
+    app: "./src/script.js",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
-  },
+  // devtool: "eval-source-map",
+  // devServer: {
+  //   watchFiles: ["./src/index.html"],
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
